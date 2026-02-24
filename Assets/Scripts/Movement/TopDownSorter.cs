4ae0a7147657a61fc091797c74f270ab6b6a971e
prima1vera <77697192+prima1vera@users.noreply.github.com>
2026-02-24 15:14:07 +0200

    Use non-alloc separation query in unit movement

--- Assets/Scripts/Movement/TopDownSorter.cs
+++ Assets/Scripts/Movement/TopDownSorter.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public class TopDownSorter : MonoBehaviour
 {
     private SpriteRenderer sr;
+    private UnitHealth unitHealth;
 
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        unitHealth = GetComponent<UnitHealth>();
     }
 
     void LateUpdate()
     {
-        if (GetComponent<UnitHealth>().CurrentState == UnitState.Dead)
+        if (sr == null || unitHealth == null)
+            return;
+
+        if (unitHealth.CurrentState == UnitState.Dead)
             return;
 
         sr.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
