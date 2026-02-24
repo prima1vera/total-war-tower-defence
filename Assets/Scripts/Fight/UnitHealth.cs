2ceabd3f4d0cf4f07519e81b81f82d79a84ba0c3
Alexander Primakov <alexander7primakov@gmail.com>
2026-02-24 12:18:55 +0200

    WIP on main Blood Update

--- Assets/Scripts/Fight/UnitHealth.cs
+++ Assets/Scripts/Fight/UnitHealth.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -55,6 +56,12 @@ public class UnitHealth : MonoBehaviour
             Die();
     }
 
+    public void SetState(UnitState newState)
+    {
+        if (CurrentState == UnitState.Dead) return;
+
+        CurrentState = newState;
+    }
 
     void Die()
     {
@@ -73,10 +80,12 @@ public class UnitHealth : MonoBehaviour
         if (bloodPoolPrefab != null)
         {
             Vector3 bloodPos = col.bounds.min;
+
             GameObject blood = Instantiate(bloodPoolPrefab, bloodPos, Quaternion.identity);
 
-            float scale = UnityEngine.Random.Range(0.3f, 1.0f);
-            blood.transform.localScale = Vector3.one * scale;
+            float targetScale = UnityEngine.Random.Range(0.3f, 1f);
+
+            StartCoroutine(AnimateBloodPool(blood.transform, targetScale));
         }
 
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
@@ -94,10 +103,31 @@ public class UnitHealth : MonoBehaviour
             col.enabled = false;
     }
 
-    public void SetState(UnitState newState)
-    {
-        if (CurrentState == UnitState.Dead) return;
 
-        CurrentState = newState;
+    IEnumerator AnimateBloodPool(Transform blood, float targetScale)
+    {
+        float duration = UnityEngine.Random.Range(2f, 8f);
+        float timer = 0f;
+
+        Vector3 startScale = Vector3.one * 0.15f;
+        Vector3 endScale = Vector3.one * targetScale;
+
+
+        blood.localScale = startScale;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            float t = timer / duration;
+
+            t = Mathf.SmoothStep(0f, 1f, t);
+
+            blood.localScale = Vector3.Lerp(startScale, endScale, t);
+
+            yield return null;
+        }
+
+        blood.localScale = endScale;
     }
 }
